1696923f0d496b8ef6eba9b9b634cb87c4f18168
Derric Fragosa <fderric@gmail.com>
2020-04-27 19:21:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -26,13 +26,7 @@ class App < Sinatra::Base
     @math_sym = params[:operation]
     @num1 = params[:number1]
     @num2 = params[:number2]
-    case = @math_sym
-    when @math_sym = "add"
-    @num1+@num2
-    # @num1-@num2.to_s
-    # @num1*@num2.to_s
-    # @num1/@num2.to_s
-    end
+  
   end
   
 end
