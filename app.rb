f2a540aa140b70ea62b6e5a535de7a21e48c52fb
Derric Fragosa <fderric@gmail.com>
2020-04-27 15:52:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-
+  
+  get '/square/:number' do
+    @square = params[:number].to_i **
+    "#{square}"
+  end
+  
+  get '/say/:number/:phrase' do
+  end
+  
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+  end
+  
+  get '/:operation/:number1/:number2' do
+  end
+  
 end
