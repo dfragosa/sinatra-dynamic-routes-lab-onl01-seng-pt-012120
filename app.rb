07938a9e2eb8487a0edb2ed2c47387d36ccb69ac
Derric Fragosa <fderric@gmail.com>
2020-04-27 18:34:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,7 +25,7 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do
     @math_sym = params[:operation]
     @num1 = params[:number1]
-    @num2 = params[:number2].to_i
+    @num2 = params[:number2]
     (@num1).+@num2.to_s
   end
   
