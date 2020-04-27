3f284495362d2e17ed830a6038733c6a2ad6cd35
Derric Fragosa <fderric@gmail.com>
2020-04-27 17:50:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do 
     @num = params[:number].to_i
     @phrase = params[:phrase].to_s
-    @num.times {|x| puts x}
+    @num.times {|x| puts x.@phrase}
    # @num.times "#{@phrase}"
   end
   
