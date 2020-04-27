d130fde4c257fe6653e276f5eab82d7facc2a06d
Derric Fragosa <fderric@gmail.com>
2020-04-27 16:11:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,9 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   
+  get '/reversename/:name' do
+  end
+  
   get '/square/:number' do
     @square = params[:number].to_i * params[:number].to_i
     "#{square}"
